4f42cf00a4d7993e3bc078537b1905b2bd4773ad
qingwangrh <qinwang@redhat.com>
2019-08-25 21:24:02 -0400

    add ins4_throttling_6disks_mix_drive_blockdev2.sh
    Signed-off-by: qingwangrh <qinwang@redhat.com>

---
+++ init.sh
@@ -0,0 +1,13 @@
+qemu-img create -f qcow2 /workdir/images/data4-1.1.qcow2 1.1G
+qemu-img create -f qcow2 /workdir/images/data4-1.2.qcow2 1.2G
+qemu-img create -f qcow2 /workdir/images/data4-1.3.qcow2 1.3G
+qemu-img create -f qcow2 /workdir/images/data4-1.4.qcow2 1.4G
+qemu-img create -f raw /workdir/images/data4-1.1.raw 1.1G
+qemu-img create -f raw /workdir/images/data4-1.2.raw 1.2G
+qemu-img create -f raw /workdir/images/data4-2.2.raw 2.2G
+qemu-img create -f raw /workdir/images/data4-2.2.raw 2.2G
+qemu-img create -f raw /workdir/images/data4-2.3.raw 2.3G
+#qemu-img create -f qcow2 /workdir/images/os4.qcow2 20G
+#qemu-img create -f qcow2 /workdir/images/os3.qcow2 20G
+#qemu-img create -f qcow2 /workdir/images/os2.qcow2 20G
+#qemu-img create -f qcow2 /workdir/images/os1.qcow2 20G
